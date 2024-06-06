228ad00aa7b754de4b2e382f1751333a093a2b9d
alpha4vit <gurinovi4rm@gmail.com>
2024-06-07 01:25:56 +0300

    added swagger docs

--- src/main/java/by/gurinovich/cryptologos/gpsolutionstechtask/dto/HotelDTO.java
+++ src/main/java/by/gurinovich/cryptologos/gpsolutionstechtask/dto/HotelDTO.java
@@ -1,15 +1,25 @@
 package by.gurinovich.cryptologos.gpsolutionstechtask.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record HotelDTO(
         Long id,
+
         String name,
+
         String description,
+
         String brand,
+
         AddressDTO address,
+
         ContactsDTO contacts,
+
         ArrivalTimeDTO arrivalTime,
+
+        @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         List<String> amenities
 ) {
 }
